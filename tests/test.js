a5c2ce56151b3318ec6c59593904ce7f7e5575e8
David Yeung <quxbaz@gmail.com>
2015-12-15 22:32:24 -0500

    Added sinon.js

---
+++ tests/test.js
@@ -0,0 +1,20 @@
+var _ = require('./underscore');
+var sinon = require('./sinon/lib/sinon');
+require('./chai').should();
+var http = require('http');
+
+describe('http.js', function() {
+
+  beforeEach(function() {
+
+  });
+
+  afterEach(function() {
+
+  });
+
+  it("test", function() {
+
+  });
+
+});
